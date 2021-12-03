999efae0147df0b1ff3f666573e1b1279babd52a
gustav0s0usa <gustarafa2005@gmail.com>
2021-12-03 19:44:11 -0300

    Criação do Formcadastro e ajustes gerais na tela de login

--- app/src/main/java/com/example/dixtech/FormLogin.java
+++ app/src/main/java/com/example/dixtech/FormLogin.java
@@ -1,10 +1,16 @@
 package com.example.dixtech;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView txttelacadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +18,21 @@ public class FormLogin extends AppCompatActivity {
         setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
 
+            txttelacadastro.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent cad = new Intent(FormLogin.this,FormCadastro.class);
+                    startActivity(cad);
+                }
+            });
 
     }
+
+    private void IniciarComponentes(){
+        txttelacadastro = findViewById(R.id.txttelacadastro);
+
+    }
+
 }
