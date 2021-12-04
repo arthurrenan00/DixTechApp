d4006d9693f9dd8977bc38c3506c3d828b090fb0
gustav0s0usa <gustarafa2005@gmail.com>
2021-12-04 18:46:02 -0300

    Layout da tela de cadastro

--- app/src/main/java/com/example/dixtech/FormCadastro.java
+++ app/src/main/java/com/example/dixtech/FormCadastro.java
@@ -2,9 +2,14 @@ package com.example.dixtech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormCadastro extends AppCompatActivity {
+
+    private TextView txttelalogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,18 @@ public class FormCadastro extends AppCompatActivity {
         setContentView(R.layout.activity_form_cadastro);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        txttelalogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent log = new Intent(FormCadastro.this,FormLogin.class);
+                startActivity(log);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        txttelalogin = findViewById(R.id.txttelalogin);
     }
 }
